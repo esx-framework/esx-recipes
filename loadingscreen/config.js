70fce17f14f4d2cf575d3c93548e543f34eaf08c
Mycroft <kaseyfitton@hotmail.co.uk>
2022-02-18 01:11:41 +0000

    Initial: Add Files

---
+++ loadingscreen/config.js
@@ -0,0 +1,20 @@
+const config = {
+    serverTitle: "{{serverName}}",
+    controls: [
+        {key:'F1', desc: 'Hud Management'},
+        {key:'F2', desc: 'Inventory'},
+        {key:'F3', desc: 'Animations'},
+        {key:'F4', desc: 'Phone'},
+        {key:'F5', desc: 'Radio'},
+        {key:'F6', desc: 'Jobs menu'},
+        {key:'F7', desc: 'Billing menu'},
+    ],
+    features: [
+        "EV-Hud",
+        "Vehicle / Weapon / Clothing / Barber shops",
+        "ESX Drugs / HoldUp",
+        "pma-voice / rp-radio",
+        "NPWD",
+        "oxmysql",
+    ],
+}
